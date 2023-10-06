e101c5997d0c9e348618eae1e25881f55dbd50c7
HerrBergenholtz <zbdv8800@gmail.com>
2023-10-06 13:24:01 +0200

    Add project files.

---
+++ Uppgift 3.3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Uppgift_3._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur länge vill du hyra bilen? (svara i hela timmar)");
+            int timmar = int.Parse(Console.ReadLine());
+            int pris = timmar * 80;
+
+            if (pris > 950)
+            {
+                Console.WriteLine("Det kommer kosta dig 950kr");
+            }
+
+            else
+            {
+                Console.WriteLine("Det kommer kosta dig " +  pris + "kr");
+            }
+            Console.ReadKey();
+        }
+    }
+}
